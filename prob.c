416de3eb13c3a9c96b2113dad38cd6944de5c855
Abraao Lincol <abraao.lincol@gamil.com>
2019-03-13 15:07:38 -0300

    Finished

--- prob.c
+++ prob.c
@@ -3,30 +3,29 @@
 #include <time.h>
 
 int main(){
-  int result[6] = {0}, n[] = {10, 1000, 10000, 100000}, r = 0;
-  float fna = 0, n1 = 0, n2 = 0;
+  int result[6] = {0}, n[] = {10, 100, 1000, 10000, 100000}, r = 0;
+  float fna = 0;
   srand(time(NULL));
 
-  for(int i = 0;i < 4;i++){
+  for(int i = 0;i < 5;i++){
 
     for(int j = 0; j < n[i]; j++){
       r = rand() % 6 + 1;
-      //printf("%d ", r);
       result[r-1] += 1;
     }
 
-    /*
-    for(int i = 0;i < 6;i++){
-      printf("%d ", result[i]);
-    }
-    printf("\n");
-    */
-
     printf("Frequencias de ocorrencias: n = %d:\n", n[i]);
     for(int z = 0;z < 6;z++){
-      fna = (float)result[z] / (float)n[i] - (1/6);
+      fna = (float)result[z] / (float)n[i];
       printf(" n%d = %0.4f", z+1, fna);
-      //result[z] = 0;
+      if( z == 2 || z == 5){
+        printf("\n");
+      }
+    }
+    printf("Diferença para a probabilidade padrão:\n");
+    for(int z = 0;z < 6;z++){
+      fna = (float)result[z] / (float)n[i] - (float)1/6;
+      printf(" n%d = %0.4f", z+1, fna);
       if( z == 2 || z == 5){
         printf("\n");
       }
